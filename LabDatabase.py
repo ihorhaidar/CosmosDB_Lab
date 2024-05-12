ff2072e2297b4461a9ac19cbcd2d877c86e62925
Ihor.Haidar <148896552+ihorhaidar@users.noreply.github.com>
2024-05-12 20:19:47 +0300

    Add files via upload

---
+++ LabDatabase.py
@@ -0,0 +1,43 @@
+from azure.cosmos import CosmosClient, PartitionKey
+
+url = 'https://it-step-cosmos-db2.documents.azure.com:443/'
+key = 'E9ziM0JVhKPeEDNoM0KYzTePmhs7cJGYn0ih28snADCfeCqz5qTLHSlwPU83qQVHweDVTMB5CMIdACDbxkzaYA=='
+
+client = CosmosClient(url, credential=key)
+
+database_name = 'LabDatabase'
+database = client.create_database_if_not_exists(id=database_name)
+
+container_name = 'LabContainer'
+container = database.create_container_if_not_exists(
+    id=container_name,
+    partition_key=PartitionKey(path="/category"),
+    offer_throughput=400
+)
+
+print("Setup complete - Database and Container are ready")
+
+for i in range(15):
+    container.upsert_item({
+        'id': str(i),
+        'category': 'SampleCategory',
+        'name': f'Item - {i}',
+        'description': f'This is a sample description N {i}'
+    })
+
+print("Documents created.")
+
+query = "SELECT * FROM c WHERE c.category='SampleCategory'"
+items = list(container.query_items(
+    query=query,
+    enable_cross_partition_query=True
+))
+
+for item in items:
+    print(item)
+
+item_to_update = items[0]
+item_to_update['description'] = 'Updated description.'
+container.replace_item(item=item_to_update['id'], body=item_to_update)
+
+
